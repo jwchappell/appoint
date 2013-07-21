6333faacbc214c2870a52e6566def03fd2d9e2a5
jwchappell <jwchappell@icloud.com>
2013-07-21 10:52:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appoint::Application.config.secret_token = 'd1b75772c9515f6ce3196f116a2c8a012a0b7e8fe113a01e10fe089454a866fa13d999269df092b2d6e46c1581692c1a75e4356da7a5c484115079771fadbf14'
